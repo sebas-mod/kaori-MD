5666b5cd0c33101c6ae1394552a6e5dd7df939f0
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 14:41:30 -0300

    Actualizar cekpsikopat.js

--- plugins/cek/cekpsikopat.js
+++ plugins/cek/cekpsikopat.js
@@ -1,9 +1,9 @@
 const pluginConfig = {
     name: 'cekpsikopat',
-    alias: ['psikopat', 'psycho'],
+    alias: ['psicopata', 'psycho'],
     category: 'cek',
-    description: 'Cek seberapa psikopat kamu',
-    usage: '.cekpsikopat <nama>',
+    description: 'Comprueba qué tan psicópata eres',
+    usage: '.cekpsikopat <nombre>',
     example: '.cekpsikopat Budi',
     isOwner: false,
     isPremium: false,
@@ -15,30 +15,30 @@ const pluginConfig = {
 }
 
 async function handler(m) {
-        const percent = Math.floor(Math.random() * 101)
+    const percent = Math.floor(Math.random() * 101)
     const mentioned = m.mentionedJid[0] || m.sender
-                    
+
     let desc = ''
     if (percent >= 90) {
-        desc = 'PSIKOPAT AKUT! Jauhi! 😈'
+        desc = '¡PSICÓPATA AGUDO! ¡Aléjense! 😈'
     } else if (percent >= 70) {
-        desc = 'Hati-hati sama orang ini 👀'
+        desc = 'Tengan cuidado con esta persona 👀'
     } else if (percent >= 50) {
-        desc = 'Ada sisi gelapnya 🌑'
+        desc = 'Tiene un lado oscuro 🌑'
     } else if (percent >= 30) {
-        desc = 'Sedikit misterius 🤔'
+        desc = 'Un poco misterioso 🤔'
     } else {
-        desc = 'Normal dan baik hati 😇'
+        desc = 'Normal y de buen corazón 😇'
     }
+
+    let txt = mentioned === m.sender ? `Hola @${mentioned.split('@')[0]}
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+Tu nivel de psicopatía es del *${percent}%*
+\`\`\`${desc}\`\`\`` : `¿Quieres comprobar el nivel de psicopatía de @${mentioned.split('@')[0]}? 
     
-Tingkat kepsikopatan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat kepsikopatan @${mentioned.split('@')[0]} yak? 
-    
-Tingkat kepsikopatan dia sebesar *${percent}%*
+Su nivel de psicopatía es del *${percent}%*
 \`\`\`${desc}\`\`\``
-    
+
     await m.reply(txt, { mentions: [mentioned] })
 }
 
