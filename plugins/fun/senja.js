5afe8d1692cd61327a20092d6417f07ed4fbb775
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 16:21:53 -0300

    Actualizar senja.js

--- plugins/fun/senja.js
+++ plugins/fun/senja.js
@@ -2,15 +2,16 @@ import axios from 'axios'
 import config from '../../config.js'
 import { f } from '../../src/lib/ourin-http.js'
 import te from '../../src/lib/ourin-error.js'
+
 const NEOXR_APIKEY = config.APIkey?.neoxr || 'Milik-Bot-OurinMD'
 
 const pluginConfig = {
-    name: 'senja',
-    alias: ['katacinta', 'romanticquotes'],
+    name: 'romantico',
+    alias: ['frasesamor', 'poesia', 'senja'],
     category: 'fun',
-    description: 'Random kata-kata senja/romantis',
-    usage: '.senja',
-    example: '.senja',
+    description: 'Genera frases románticas o poéticas al azar',
+    usage: '.romantico',
+    example: '.romantico',
     isOwner: false,
     isPremium: false,
     isGroup: false,
@@ -24,11 +25,12 @@ async function handler(m, { sock }) {
     m.react('🕕')
     try {
         const res = await f(`https://api.neoxr.eu/api/senja?apikey=${NEOXR_APIKEY}`)
-        
+
         if (!res.status || !res.data?.text) {
             m.react('❌')
-            return m.reply(`❌ Gagal mengambil kata senja`)
+            return m.reply(`❌ No pude obtener la frase romántica.`)
         }
+        
         await m.reply(res.data.text)
         m.react('✅')
     } catch (err) {
