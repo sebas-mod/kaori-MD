27b1738479a5d649528ee172bcf90308410b83a9
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 04:57:30 -0300

    Update cekkpopers.js

--- plugins/cek/cekkpopers.js
+++ plugins/cek/cekkpopers.js
@@ -1,10 +1,10 @@
 const pluginConfig = {
-    name: 'cekkpopers',
-    alias: ['kpopers', 'kpop'],
+    name: 'chequekpop',
+    alias: ['kpopers', 'kpop', 'stan'],
     category: 'cek',
-    description: 'Cek tingkat kpopers kamu',
-    usage: '.cekkpopers <nama>',
-    example: '.cekkpopers Budi',
+    description: 'Verifica qué tan kpopers sos',
+    usage: '.chequekpop <nombre>',
+    example: '.chechequekpop @usuario',
     isOwner: false,
     isPremium: false,
     isGroup: false,
@@ -15,22 +15,22 @@ const pluginConfig = {
 }
 
 async function handler(m) {
-        const percent = Math.floor(Math.random() * 101)
+    const percent = Math.floor(Math.random() * 101)
     const mentioned = m.mentionedJid[0] || m.sender
                     
     let desc = ''
-    if (percent >= 90) desc = 'ARMY/BLINK level max! 💜💗'
-    else if (percent >= 70) desc = 'Stan berat nih! 🎤'
-    else if (percent >= 50) desc = 'Casual listener~ 🎵'
-    else if (percent >= 30) desc = 'Tau dikit-dikit aja 😅'
-    else desc = 'Bukan kpopers 🤷'
+    if (percent >= 90) desc = '¡Nivel ARMY/BLINK máximo! 💜💗'
+    else if (percent >= 70) desc = '¡Sos re fan! Stan de pura cepa 🎤'
+    else if (percent >= 50) desc = 'Oyente casual~ 🎵'
+    else if (percent >= 30) desc = 'Conocés un par de temas nada más 😅'
+    else desc = 'No sos kpopers 🤷'
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+    let txt = mentioned === m.sender ? `Hola @${mentioned.split('@')[0]}
     
-Tingkat kekpopersan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat kekpopersan @${mentioned.split('@')[0]} yak? 
+Tu nivel de kpopers es del *${percent}%*
+\`\`\`${desc}\`\`\`` : `¿Querés chequear el nivel de kpopers de @${mentioned.split('@')[0]}? 
     
-Tingkat kekpopersan dia sebesar *${percent}%*
+Su nivel de kpopers es del *${percent}%*
 \`\`\`${desc}\`\`\``
     
     await m.reply(txt, { mentions: [mentioned] })
