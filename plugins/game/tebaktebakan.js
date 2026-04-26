e29970560692e15170b4d8b7e917b6f6cfed739c
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 16:49:18 -0300

    Actualizar tebaktebakan.js

--- plugins/game/tebaktebakan.js
+++ plugins/game/tebaktebakan.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebaktebakan', {
-    alias: ['tbt', 'tebak2an', 'receh'],
+    alias: ['tbt', 'chiste', 'tebak2an', 'receh'],
     emoji: '😄',
-    title: 'TEBAK-TEBAKAN',
-    description: 'Tebak-tebakan receh'
+    title: 'ADIVINANZAS GRACIOSAS',
+    description: 'Adivinanzas simples y chistes para pasar el rato'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebaktebakan')
