a00a9f522dab1aa8277cf0aa4333b6f85089b4c7
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 16:37:00 -0300

    Actualizar siapakahaku.js

--- plugins/game/siapakahaku.js
+++ plugins/game/siapakahaku.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('siapakahaku', {
-    alias: ['siapa', 'whoami'],
+    alias: ['quiensoy', 'siapa', 'whoami'],
     emoji: '🎭',
-    title: 'SIAPAKAH AKU',
-    description: 'Tebak dari deskripsi'
+    title: '¿QUIÉN SOY?',
+    description: 'Adivina el personaje u objeto según la descripción'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('siapakahaku')
