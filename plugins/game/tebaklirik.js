276f6e51d6af613f85e0e514fbbfc6640aef2993
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 16:46:54 -0300

    Actualizar tebaklirik.js

--- plugins/game/tebaklirik.js
+++ plugins/game/tebaklirik.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebaklirik', {
-    alias: [],
+    alias: ['lirik', 'letra', 'letracancion'],
     emoji: '🎤',
-    title: 'TEBAK LIRIK',
-    description: 'Tebak lirik lagu'
+    title: 'ADIVINA LA LETRA',
+    description: 'Adiviná la letra de la canción'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebaklirik')
