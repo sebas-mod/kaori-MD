9a1446295a6195dd50159dc8aec2949216900c58
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 14:43:58 -0300

    Actualizar ceksexy.js

--- plugins/cek/ceksexy.js
+++ plugins/cek/ceksexy.js
@@ -2,8 +2,8 @@ const pluginConfig = {
     name: 'ceksexy',
     alias: ['sexy', 'hot'],
     category: 'cek',
-    description: 'Cek seberapa sexy kamu',
-    usage: '.ceksexy <nama>',
+    description: 'Comprueba qué tan sexy eres',
+    usage: '.ceksexy <nombre>',
     example: '.ceksexy Budi',
     isOwner: false,
     isPremium: false,
@@ -15,30 +15,30 @@ const pluginConfig = {
 }
 
 async function handler(m) {
-        const percent = Math.floor(Math.random() * 101)
+    const percent = Math.floor(Math.random() * 101)
     const mentioned = m.mentionedJid[0] || m.sender
-                    
+
     let desc = ''
     if (percent >= 90) {
-        desc = 'SEXY ABIS! 🔥🔥🔥'
+        desc = '¡DEMASIADO SEXY! 🔥🔥🔥'
     } else if (percent >= 70) {
-        desc = 'Hot banget! 😏'
+        desc = '¡Muy hot! 😏'
     } else if (percent >= 50) {
-        desc = 'Lumayan menggoda~ 😊'
+        desc = 'Bastante tentador~ 😊'
     } else if (percent >= 30) {
-        desc = 'Biasa aja sih 🙂'
+        desc = 'Normalito, la verdad 🙂'
     } else {
-        desc = 'Mungkin cute bukan sexy 😅'
+        desc = 'Quizás eres más cute que sexy 😅'
     }
+
+    let txt = mentioned === m.sender ? `Hola @${mentioned.split('@')[0]}
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+Tu nivel de sexapil es del *${percent}%*
+\`\`\`${desc}\`\`\`` : `¿Quieres comprobar qué tan sexy es @${mentioned.split('@')[0]}? 
     
-Tingkat kesexyan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat kesexyan @${mentioned.split('@')[0]} yak? 
-    
-Tingkat kesexyan dia sebesar *${percent}%*
+Su nivel de sexapil es del *${percent}%*
 \`\`\`${desc}\`\`\``
-    
+
     await m.reply(txt, { mentions: [mentioned] })
 }
 
