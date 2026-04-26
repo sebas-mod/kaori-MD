19b6660f8f3d42637468367948289f7184839423
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-26 16:48:35 -0300

    Actualizar tebakprofesi.js

--- plugins/game/tebakprofesi.js
+++ plugins/game/tebakprofesi.js
@@ -1,10 +1,10 @@
 import { games } from '../../src/lib/ourin-games.js'
 
 games.register('tebakprofesi', {
-    alias: ['tp', 'guessjob'],
+    alias: ['tp', 'profesion', 'guessjob', 'empleo'],
     emoji: '👨‍💼',
-    title: 'TEBAK PROFESI',
-    description: 'Tebak nama profesi'
+    title: 'ADIVINA LA PROFESIÓN',
+    description: 'Adiviná el nombre de la profesión'
 })
 
 const { config: pluginConfig, handler, answerHandler } = games.createPlugin('tebakprofesi')
