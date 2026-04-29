069d56ec39cef232baf08d92c4cb614ae4bccde3
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-29 11:27:18 -0300

    Update listban.js

--- plugins/owner/listban.js
+++ plugins/owner/listban.js
@@ -1,12 +1,13 @@
 import config from '../../config.js'
 import { getDatabase } from '../../src/lib/ourin-database.js'
+
 const pluginConfig = {
-    name: 'listban',
-    alias: ['listbanned', 'banlist'],
+    name: 'listaban',
+    alias: ['listabanned', 'banlist', 'baneados'],
     category: 'owner',
-    description: 'Melihat daftar banned user',
-    usage: '.listban',
-    example: '.listban',
+    description: 'Ver la lista de usuarios baneados',
+    usage: '.listaban',
+    example: '.listaban',
     isOwner: true,
     isPremium: false,
     isGroup: false,
@@ -21,18 +22,18 @@ async function handler(m, { sock }) {
     const bannedUsers = config.bannedUsers && config.bannedUsers.length > 0 ? config.bannedUsers : (db.setting('bannedUsers') || [])
     
     if (bannedUsers.length === 0) {
-        return m.reply(`🚫 *ʟɪsᴛ ʙᴀɴɴᴇᴅ*\n\n> Tidak ada user yang dibanned\n\n\`Gunakan: ${m.prefix}ban <nomor>\``)
+        return m.reply(`🚫 *ʟɪsᴛᴀ ᴅᴇ ʙᴀɴᴇᴀᴅᴏs*\n\n> No hay usuarios baneados actualmente\n\n\`Usa: ${m.prefix}ban <número>\``)
     }
     
-    let caption = `🚫 *ʟɪsᴛ ʙᴀɴɴᴇᴅ*\n\n`
-    caption += `╭┈┈⬡「 ⛔ *ᴜsᴇʀs* 」\n`
+    let caption = `🚫 *ʟɪsᴛᴀ ᴅᴇ ʙᴀɴᴇᴀᴅᴏs*\n\n`
+    caption += `╭┈┈⬡「 ⛔ *ᴜsᴜᴀʀɪᴏs* 」\n`
     
     for (let i = 0; i < bannedUsers.length; i++) {
         caption += `┃ ${i + 1}. \`${bannedUsers[i]}\`\n`
     }
     
     caption += `╰┈┈⬡\n\n`
-    caption += `> ᴛᴏᴛᴀʟ: \`${bannedUsers.length}\` ʙᴀɴɴᴇᴅ ᴜsᴇʀ`
+    caption += `> ᴛᴏᴛᴀʟ: \`${bannedUsers.length}\` ᴜsᴜᴀʀɪᴏs ʙᴀɴᴇᴀᴅᴏs`
     
     await m.reply(caption)
 }
